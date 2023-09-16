57f457bf12dbc109ce47b617e2a0e2ebbea5f720
haeyongkimbr-usp <55086625+haeyongkimbr-usp@users.noreply.github.com>
2023-09-16 10:49:28 -0300

    Update vgg16.py

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -1,6 +1,6 @@
-import keras
-from keras.models import *
-from keras.layers import *
+import tensorflow.keras as keras
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
 import tensorflow as tf
 from .config import IMAGE_ORDERING
 
